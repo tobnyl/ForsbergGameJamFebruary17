be2b57c026c22289dfe0636fea9a7a503858d35f
tobnyl <tobnyl@gmail.com>
2017-02-25 12:58:55 +0100

    Roll and forward force

--- ForsbergGameJamFebruary17/Assets/Scripts/Spaceship.cs
+++ ForsbergGameJamFebruary17/Assets/Scripts/Spaceship.cs
@@ -7,8 +7,10 @@ public class Spaceship : MonoBehaviour {
 	#region Fields/Properties
 
 	public int Index = 1;
+	public float ForwardForce = 1;
 	public float PitchTorque = 1;
 	public float YawTorque = 1;
+	public float RollTorque = 1;
 
 	private Rigidbody _rigidbody;
 
@@ -20,6 +22,11 @@ public class Spaceship : MonoBehaviour {
 	private Vector2 AxisRight
 	{
 		get { return new Vector3(Input.GetAxis("HorizontalRight" + Index), Input.GetAxis("VerticalRight" + Index)); }
+	}
+
+	private float Trigger
+	{
+		get { return Input.GetAxis("TriggerRight1") * -1; }
 	}
 
 	#endregion
@@ -37,15 +44,27 @@ public class Spaceship : MonoBehaviour {
 
 	void Update()
 	{
-		Debug.LogFormat("AxisLeft: ({0}, {1})", AxisLeft.x, AxisLeft.y);
-		Debug.LogFormat("AxisRight: ({0}, {1})", AxisRight.x, AxisRight.y);
+		//Debug.LogFormat("AxisLeft: ({0}, {1})", AxisLeft.x, AxisLeft.y);
+		//Debug.LogFormat("AxisRight: ({0}, {1})", AxisRight.x, AxisRight.y);
 	}
 
 	void FixedUpdate()
 	{
+		_rigidbody.AddForce(transform.forward * Trigger * ForwardForce);
 
-		_rigidbody.AddTorque(transform.right * AxisLeft.y * PitchTorque);
-		_rigidbody.AddTorque(Vector3.up * AxisLeft.x * YawTorque);
+
+		if (Mathf.Abs(AxisLeft.y) > 0.2)
+		{
+			_rigidbody.AddTorque(transform.right * AxisLeft.y * PitchTorque);
+		}
+		if (Mathf.Abs(AxisLeft.x) > 0.2)
+		{
+			_rigidbody.AddTorque(transform.up * AxisLeft.x * YawTorque);
+		}
+		if (Mathf.Abs(AxisRight.x) > 0.2)
+		{
+			_rigidbody.AddTorque(transform.forward * (-AxisRight.x) * RollTorque);
+		}
 	}
 
 	#endregion
