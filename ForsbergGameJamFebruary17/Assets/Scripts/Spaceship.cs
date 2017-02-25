e1f663d703d9c2594ff196a91a5b6211e1dfb368
tobnyl <tobnyl@gmail.com>
2017-02-25 15:00:27 +0100

    Save

--- ForsbergGameJamFebruary17/Assets/Scripts/Spaceship.cs
+++ ForsbergGameJamFebruary17/Assets/Scripts/Spaceship.cs
@@ -7,12 +7,21 @@ public class Spaceship : MonoBehaviour {
 	#region Fields/Properties
 
 	public int Index = 1;
+
+	[Header("Force")]
 	public float ForwardForce = 1;
+
+	[Header("Torque")]
 	public float PitchTorque = 1;
 	public float YawTorque = 1;
 	public float RollTorque = 1;
 
+	[Header("Max")]
+	public float MaxVelocity = 20;
+	public float MaxAngularVelocity = 1;
+
 	private Rigidbody _rigidbody;
+	private Transform _transform;
 
 	private Vector2 AxisLeft
 	{
@@ -35,26 +44,37 @@ public class Spaceship : MonoBehaviour {
 	void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
+		_rigidbody.maxAngularVelocity = MaxAngularVelocity;
 	}
 
 	void Start()
 	{
-
+		_transform = transform;
 	}
 
 	void Update()
 	{
+		Debug.Log(Trigger);
+
 		//Debug.LogFormat("AxisLeft: ({0}, {1})", AxisLeft.x, AxisLeft.y);
 		//Debug.LogFormat("AxisRight: ({0}, {1})", AxisRight.x, AxisRight.y);
+		//Debug.LogFormat("Velocity: {0}", _rigidbody.velocity);
 	}
 
 	void FixedUpdate()
 	{
+		if (_rigidbody.velocity.magnitude > MaxVelocity)
+		{
+			_rigidbody.velocity = _rigidbody.velocity.normalized * MaxVelocity;
+		}
+
 		_rigidbody.AddForce(transform.forward * Trigger * ForwardForce);
 
-		_rigidbody.AddTorque(transform.right * AxisLeft.y * PitchTorque);
+		//_rigidbody.AddTorque(transform.right * AxisLeft.y * PitchTorque);
 		_rigidbody.AddTorque(transform.up * AxisLeft.x * YawTorque);
-		_rigidbody.AddTorque(transform.forward * (-AxisRight.x) * RollTorque);
+		//_rigidbody.AddTorque(transform.forward * (-AxisLeft.x) * RollTorque);
+
+
 	}
 
 	#endregion
