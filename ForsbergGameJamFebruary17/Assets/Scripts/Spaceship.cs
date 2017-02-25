254657ae3a6e534f1b6470cc2c312fd5e178372c
tobnyl <tobnyl@gmail.com>
2017-02-25 12:09:16 +0100

    Spaceship

---
+++ ForsbergGameJamFebruary17/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour {
+
+	#region Fields/Properties
+
+	private Rigidbody _rigidbody;
+
+	#endregion
+	#region Events
+
+	void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		_rigidbody.AddForce(transform.forward * 10f);
+	}
+
+	#endregion
+	#region Methods
+
+
+
+	#endregion
+}
