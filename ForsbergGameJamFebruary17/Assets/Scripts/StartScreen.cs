6925aa21aeb5f266143756df5b9422e873dad607
tobnyl <tobnyl@gmail.com>
2017-02-26 20:51:43 +0100

    Start screen done!

---
+++ ForsbergGameJamFebruary17/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour 
+{
+	#region Fields/Properties
+
+	
+	
+	#endregion
+	#region Events
+	
+	void Awake()
+	{
+		
+	}
+	
+	void Start() 
+	{
+	
+	}
+
+	void Update() 
+	{
+		if (Input.GetButtonDown("Submit"))
+		{
+			SceneManager.LoadScene("Main");
+		}
+
+		if (Input.GetButtonDown("Cancel"))
+		{
+			Debug.Log("Yes?");
+
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#endif
+
+			Application.Quit();
+		}
+	}
+	
+	#endregion
+	#region Methods
+	
+	
+	
+	#endregion
+	
+}
